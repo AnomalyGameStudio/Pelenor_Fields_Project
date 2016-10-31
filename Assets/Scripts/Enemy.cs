39378e8c1b2f1c1f6d05f54c46973210a850dbb0
elopes42 <elopes06@gmail.com>
2016-10-31 17:11:53 -0200

    First Commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour
+{
+    GameObject pathGO;
+
+    Transform targetPathNode;
+    int pathNodeIndex = 0;
+
+    public float speed = 5f;
+    public float health = 1;
+    public int moneyValue = 1;
+
+    // Use this for initialization
+    void Start ()
+    {
+        pathGO = GameObject.Find("Path");
+	}
+	
+    void GetNextPathNode()
+    {
+        if (pathNodeIndex < pathGO.transform.childCount)
+        {
+            targetPathNode = pathGO.transform.GetChild(pathNodeIndex);
+            pathNodeIndex++;
+        }
+        else
+        {
+            targetPathNode = null;
+            ReachedGoal();
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(targetPathNode == null)
+        {
+            GetNextPathNode();
+            if(targetPathNode == null)
+            {
+                // Run out of Path;
+                ReachedGoal();
+                return;
+            }
+        }
+
+        Vector3 dir = targetPathNode.position - this.transform.localPosition;
+
+        float distThisFrame = speed * Time.deltaTime;
+
+        if(dir.magnitude <= distThisFrame)
+        {
+            // We reached the node
+            targetPathNode = null;
+
+        }
+        else
+        {
+            // TODO: Consider ways to smooth this motion.
+
+            // Mode towards node
+            transform.Translate(dir.normalized * distThisFrame, Space.World);
+            Quaternion targetRotation = Quaternion.LookRotation(dir);
+            this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * 5);
+        }
+	}
+
+
+    void ReachedGoal()
+    {
+        GameObject.FindObjectOfType<ScoreManager>().LoseLife();
+        Destroy(gameObject);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+
+        if(health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // TODO: Do this more safely;
+        GameObject.FindObjectOfType<ScoreManager>().money += moneyValue;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+    Transform turretTransform;
+
+    public GameObject bulletPrefab;
+    public int cost = 5;
+
+    public float range = 10f;
+    public float damage = 1;
+    public float radius = 0;
+
+    float fireCooldown = 0.5f;
+    float fireCooldownLeft = 0;
+
+	// Use this for initialization
+	void Start () {
+        turretTransform = transform.Find("Turret");
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        // TODO: Optimize this!
+
+        Enemy[] enemies = GameObject.FindObjectsOfType<Enemy>();
+
+        Enemy nearestEnemy = null;
+        float dist = Mathf.Infinity;
+
+        foreach(Enemy e in enemies)
+        {
+            float d = Vector3.Distance(this.transform.position, e.transform.position);
+
+            if(nearestEnemy == null || d < dist)
+            {
+                nearestEnemy = e;
+                dist = d;
+            }   
+        }
+
+        if (nearestEnemy == null)
+        {
+            Debug.Log("No Enemies");
+            return;
+        }
+
+        Vector3 dir = nearestEnemy.transform.position - this.transform.position;
+        Quaternion lookRot = Quaternion.LookRotation(dir);
+
+        turretTransform.rotation = Quaternion.Euler(0, lookRot.eulerAngles.y, 0);
+
+        fireCooldownLeft -= Time.deltaTime;
+
+        if(fireCooldownLeft <= 0 && dir.magnitude <= range)
+        {
+            fireCooldownLeft = fireCooldown;
+            ShootAt(nearestEnemy);
+        }
+    }
+
+    void ShootAt(Enemy e)
+    {
+        // TODO: Fire out the tip!
+        GameObject bulletGO = (GameObject)Instantiate(bulletPrefab, this.transform.position, this.transform.rotation);
+
+        Bullet b = bulletGO.GetComponent<Bullet>();
+        b.target = e.transform;
+        b.damage = damage;
+        b.radius = radius;
+    }
+
+
+    // TODO: TOWER UPGRADES
+
+    // Manager for tower upgrade. When player clicks on the tower, the tower tells the manager that it is active, the manager show the stats of the tower
+    // Add a collider to the tower so it can use the OnMouseDown/OnMouseUp
+}
