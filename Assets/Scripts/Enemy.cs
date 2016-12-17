293fe3a5e1bb54796e1c58a1a26e0f633428243b
elopes42 <elopes06@gmail.com>
2016-12-17 20:21:25 -0200

    Movement code comented for future reference

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,10 +3,10 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour, IDamageable
 {
-    GameObject pathGO;
+    //GameObject pathGO;
 
-    Transform targetPathNode;
-    int pathNodeIndex = 0;
+    //Transform targetPathNode;
+    //int pathNodeIndex = 0;
 
     public float speed = 5f;
     public float health = 1;
@@ -15,9 +15,10 @@ public class Enemy : MonoBehaviour, IDamageable
     // Use this for initialization
     void Start ()
     {
-        pathGO = GameObject.Find("Path");
+        //pathGO = GameObject.Find("Path");
 	}
 	
+    /*
     void GetNextPathNode()
     {
         if (pathNodeIndex < pathGO.transform.childCount)
@@ -35,25 +36,30 @@ public class Enemy : MonoBehaviour, IDamageable
             
         }
     }
+    */
 
+    
+    
 	// Update is called once per frame
 	void Update ()
     {
+        /*
 	    if(targetPathNode == null)
         {
             GetNextPathNode();
             if(targetPathNode == null)
             {
                 // Run out of Path;
-                ReachedGoal();
+                //ReachedGoal();
                 return;
             }
         }
+        */
+        //Vector3 dir = targetPathNode.position - this.transform.localPosition;
 
-        Vector3 dir = targetPathNode.position - this.transform.localPosition;
+        //float distThisFrame = speed * Time.deltaTime;
 
-        float distThisFrame = speed * Time.deltaTime;
-
+        /*
         if(dir.magnitude <= distThisFrame)
         {
             // We reached the node
@@ -69,14 +75,16 @@ public class Enemy : MonoBehaviour, IDamageable
             Quaternion targetRotation = Quaternion.LookRotation(dir);
             this.transform.rotation = Quaternion.Lerp(this.transform.rotation, targetRotation, Time.deltaTime * 5);
         }
+        */
 	}
 
-
+    /*
     void ReachedGoal()
     {
         GameObject.FindObjectOfType<ScoreManager>().LoseLife();
         Destroy(gameObject);
     }
+    */
 
     public void TakeDamage(float damage)
     {
