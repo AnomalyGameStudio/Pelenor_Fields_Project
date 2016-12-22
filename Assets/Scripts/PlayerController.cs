27ec5460652cfc4d29ea2be93423c829b7565e18
elopes42 <elopes06@gmail.com>
2016-12-22 00:12:00 -0200

    Added the shield and damage components
    Added the shield regenerator overtime and adjusted the TakeDamage
    routine to start taking damage from the shield before the hull

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,30 +19,47 @@ public class PlayerController : MonoBehaviour, IDamageable
     {
         currentShield = shield;
         nextTimeRecharge = Time.time + RechargeCooldown;
+
+        ScoreManager.Instance.Shield = (int) currentShield;
     }
 
     void Update()
     {
         // Recharge the shield
-
+        rechargeShield();
+        
     }
 
 	public void TakeDamage(float damage)
     {
-        ScoreManager.Instance.LoseLife ((int) damage);
+        if(currentShield > 0)
+        {
+            currentShield -= damage;
+            nextTimeRecharge = Time.time + RechargeCooldown;
+            ScoreManager.Instance.Shield = (int) currentShield;
+
+            // TODO add a clamp to the damage.
+        }
+        else
+        {
+            ScoreManager.Instance.LoseLife((int)damage);
+        }
+        //ScoreManager.Instance.LoseLife ((int) damage);
     }
 
     // TODO Verificar se vai ser usado publicamente, enquanto isso deixar publico
 
     private void rechargeShield()
     {
-        if(Time.time < nextTimeRecharge && currentShield < shield)
+        if(Time.time > nextTimeRecharge && currentShield < shield)
         {
+            
             // TODO Update UI... Not Idea how this will work
             currentShield += shieldRechargeRate * Time.deltaTime;
             
             // TODO See if this is correct. Might need to see the documentation
             currentShield = Mathf.Clamp(currentShield, 0, shield);
+            ScoreManager.Instance.Shield = (int)currentShield;
         }
     }
 }
