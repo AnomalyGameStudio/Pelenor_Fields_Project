71ce5efbd3d4d137640ebf399507381b108ceeee
elopes42 <elopes06@gmail.com>
2017-01-01 14:31:37 -0200

    Create script that will be used to create a level list
    Create script that will be used to create a level list

---
+++ Assets/LevelList.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LevelList : MonoBehaviour
+{
+    public GameObject buttonPrefab;
+    public GameObject parent;
+
+	
+	void Start ()
+    {
+        // Instantiate the Button prefab
+        GameObject buttonGO = Instantiate(buttonPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+        // Sets the Parent as the Grid in order for it to be scrollable
+        buttonGO.transform.parent = parent.transform;
+
+        // Gets the button component and Adds to the Listener the LoadLevel method
+        Button button = buttonGO.GetComponent<Button>();
+        button.onClick.AddListener(LoadLevel);
+
+        // Change the Text Label of the Button
+        button.GetComponentInChildren<Text>().text = "Level 3";
+    }
+	
+    public void LoadLevel()
+    {
+        // Can use this to pass the values I need to the level loader in the scene
+        LevelLoader.instance.level = "Level3";
+        // Use the Event Manager to Load the new Level
+        EventManager.TriggerEvent("LoadLevel");
+    }
+
+}
